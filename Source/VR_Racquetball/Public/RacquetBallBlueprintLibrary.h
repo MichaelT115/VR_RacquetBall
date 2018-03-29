dd0c9c9e2175ef9af026b82c5469411c3394d315
Michael Cooper <mtc5284@rit.edu>
2018-03-28 22:17:42 -0400

    Add C++

---
+++ Source/VR_Racquetball/Public/RacquetBallBlueprintLibrary.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "RacquetBallBlueprintLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VR_RACQUETBALL_API URacquetBallBlueprintLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
